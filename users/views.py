7fb089d75afdb6f4098fa868f9f0721f9e16d47d
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-24 00:00:56 +0530

    5-create super user

--- users/views.py
+++ users/views.py
@@ -4,17 +4,21 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 # Create your views here.
 def user_login(request):
-    if request.method == 'POSt':
+    if request.method == 'POST':
         form = LoginForm(request.POST)
+        print("form",form)
         if form.is_vaild():
             data = form.cleaned_data
+            print('data',data)
             user = authenticate(request, username = data['username'], password = data['password'])
             if user is not None:
                 login(request, user)
                 return HttpResponse('User Logged In')
             else:
                 return HttpResponse('Invail Login')
+        else:
+            print('not vaild')
     else:
         form = LoginForm()
 
-        return render(request, 'users/login.html', {'form':form})
+    return render(request, 'users/login.html', {'form':form})
