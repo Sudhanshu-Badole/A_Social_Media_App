2671e22bb5bc92195efd9e1db1187d7be0853109
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-28 00:08:01 +0530

    13-Password Reset Complete View"

--- users/urls.py
+++ users/urls.py
@@ -17,5 +17,7 @@ urlpatterns = [
          name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('reset/done', auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
