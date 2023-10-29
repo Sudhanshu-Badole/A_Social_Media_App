26b1a45b6f7bc2e0db35f81a45428bae05f6526d
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-29 08:24:25 +0530

    16-Extending User Model

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -126,3 +126,6 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
