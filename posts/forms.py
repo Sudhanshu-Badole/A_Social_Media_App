253e5fea404ef300a04fd0ee56f1ba0d3ccc1e9d
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-11-02 23:52:51 +0530

    20-Creating Post

---
+++ posts/forms.py
@@ -0,0 +1,8 @@
+from .models import Post
+from django import forms
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'image', 'caption')
+        
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import PostCreateForm
 # Create your views here.
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostCreateForm(data=request.POST)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+    else:
+        form = PostCreateForm(data=request.POST)
+
+    return render(request, 'posts/create.html', {'form': form})
